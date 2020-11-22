dda9cbcdbcffd0a3660ca355e8642d71ffa6da50
Dhinesh <vijayadinesh93@gmail.com>
2020-11-22 14:32:48 +0530

    code files

---
+++ config.py
@@ -0,0 +1,9 @@
+host_config = {
+    "host": "127.0.0.1",
+    "port": 1947,
+    "debug": True,
+    "threaded": True
+}
+
+request_url = "redis://localhost:6379/0"
+message = f"Find your file @ <code>{}</code>"
